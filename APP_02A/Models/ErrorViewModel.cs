0eaef228df69b0532e0448808b565ceff929a580
Lenny <lenncode@gmail.com>
2023-09-20 20:42:33 -0500

    Agregar archivos de proyecto.

---
+++ APP_02A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APP_02A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
